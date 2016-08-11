921e7332a1370624b1fd81f42a7cc7024d54b903
Raman Prasad <raman_prasad@harvard.edu>
2016-08-11 08:25:41 -0400

    added view caching for viz pages

--- dv_apps/metrics/views_public_metrics.py
+++ dv_apps/metrics/views_public_metrics.py
@@ -16,7 +16,7 @@ from dv_apps.metrics.stats_util_datasets import StatsMakerDatasets
 from dv_apps.metrics.stats_util_dataverses import StatsMakerDataverses
 from dv_apps.metrics.stats_util_files import StatsMakerFiles
 
-#@cache_page(60 * 15)
+@cache_page(60 * 60 * 3)
 def view_public_visualizations_last12(request):
     """
     Return visualizations covering the last 12 months+.
@@ -32,7 +32,7 @@ def view_public_visualizations_last12(request):
     return view_public_visualizations(request, **date_filters)
 
 
-#@cache_page(60 * 15)
+@cache_page(60 * 60 * 3)
 def view_public_visualizations(request, **kwargs):
     """
     Return HTML/D3Plus visualizations for a variety of public statistics

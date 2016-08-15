c93c878c4d9469aa1495503a67af2ccc67adb3e7
Raman Prasad <raman_prasad@harvard.edu>
2016-08-15 14:08:24 -0400

    remove gzip from whitenoise

--- miniverse/settings/heroku_dev.py
+++ miniverse/settings/heroku_dev.py
@@ -50,4 +50,4 @@ STATICFILES_DIRS = (
     join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
---
+++ staticfiles/PLACEHOLDER.md
@@ -0,0 +1,1 @@
+place holder to have staticfiles directory

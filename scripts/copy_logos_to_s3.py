4107c16a4fa4dba2e5fe0d1c9a9012e1d3a0d42f
Raman Prasad <raman_prasad@harvard.edu>
2016-08-25 16:50:49 -0400

    blowing away accidentally uploaded creds

--- scripts/copy_logos_to_s3.py
+++ scripts/copy_logos_to_s3.py
@@ -7,12 +7,12 @@ from os.path import join
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-aws_bucket_url = 'https://bucketeer-38679028-08e1-4038-bf0e-bb761d97f8d7.s3.amazonaws.com/public/'
-aws_access_key = 'AKIAI52EVLMQHCJOXZGQ'
-aws_secret = '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'
+aws_bucket_url = 'the-url'
+aws_access_key = 'ok-there'
+aws_secret = 'blah'
 conn = S3Connection(aws_access_key, aws_secret)
 
-bucket_name = 'bucketeer-38679028-08e1-4038-bf0e-bb761d97f8d7'
+bucket_name = 'bucket-name'
 bucket = conn.create_bucket(bucket_name)
 
 def list_items():

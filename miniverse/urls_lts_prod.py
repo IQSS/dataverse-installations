c787c6e40457973d4a1638e73062ba23004b7ab1
Raman Prasad <raman_prasad@harvard.edu>
2016-08-31 15:44:03 -0400

    same as last one..without err

--- miniverse/urls_lts_prod.py
+++ miniverse/urls_lts_prod.py
@@ -11,16 +11,16 @@ admin.site.site_header = 'Dataverse Metrics (Miniverse)'
 urlpatterns = [
     # Examples:
     # url(r'^$', 'miniverse.views.home', name='home'),
-    #url(r'^dr2m/', include('dv_apps.dvobjects.urls')),
+    url(r'^dr2m/', include('dv_apps.dvobjects.urls')),
 
-    #url(r'^dataset/', include('dv_apps.datasets.urls')),
+    url(r'^dataset/', include('dv_apps.datasets.urls')),
 
     url(r'^miniverse/map/', include('dv_apps.installations.urls')),
 
     url(r'^miniverse/metrics/', include('dv_apps.metrics.urls')),
 
     # temp experiment
-    #url(r'^dvobjects/', include('dv_apps.dvobject_api.urls')),
+    url(r'^dvobjects/', include('dv_apps.dvobject_api.urls')),
 
     url(r'^miniverse/miniverse-admin/', include(admin.site.urls)),
 

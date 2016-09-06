7edd4842b201312f98da6cf40ac49354ed8b33a5
Raman Prasad <raman_prasad@harvard.edu>
2016-09-06 14:41:46 -0400

    redo prod urls

--- miniverse/urls_lts_prod.py
+++ miniverse/urls_lts_prod.py
@@ -8,21 +8,21 @@ from django.conf.urls.static import static
 
 admin.site.site_header = 'Dataverse Metrics (Miniverse)'
 
-URL_PREFIX = ''
+URL_PREFIX = 'miniverse/'
 
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'miniverse.views.home', name='home'),
-    url(r'^%sdr2m/' % URL_PREFIX, include('dv_apps.dvobjects.urls')),
+    #url(r'^%sdr2m/' % URL_PREFIX, include('dv_apps.dvobjects.urls')),
 
-    url(r'^%sdataset/' % URL_PREFIX, include('dv_apps.datasets.urls')),
+    #url(r'^%sdataset/' % URL_PREFIX, include('dv_apps.datasets.urls')),
 
     url(r'^%smap/' % URL_PREFIX, include('dv_apps.installations.urls')),
 
     url(r'^%smetrics/' % URL_PREFIX, include('dv_apps.metrics.urls')),
 
-    url(r'^%sdvobjects/' % URL_PREFIX, include('dv_apps.dvobject_api.urls')),
+    #url(r'^%sdvobjects/' % URL_PREFIX, include('dv_apps.dvobject_api.urls')),
 
     url(r'^%sminiverse-admin/' % URL_PREFIX, include(admin.site.urls)),
 

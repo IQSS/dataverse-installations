49ce5e71a6e85233618d3f62e2edfa483f769750
Raman Prasad <raman_prasad@harvard.edu>
2017-01-24 16:46:03 -0500

    updated links

--- scripts/scriptlet.js
+++ scripts/scriptlet.js
@@ -1,1 +1,3 @@
 javascript:var myParam = location.search.split('persistentId=')[1];window.location = 'https://services.dataverse.harvard.edu/miniverse/dvobjects/api/v1/datasets/by-persistent-id?persistentId=' + myParam;
+
+javascript:var myParam = location.search.split('fileId=')[1];window.location = 'https://services.dataverse.harvard.edu/miniverse/datafile/table-preview-html/' + myParam;

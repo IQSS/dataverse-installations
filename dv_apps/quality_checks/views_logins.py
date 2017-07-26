a6413712968d5ab241849fce1ac0c963527db33c
Raman Prasad <raman_prasad@harvard.edu>
2017-07-26 16:12:38 -0400

    add cache

--- dv_apps/quality_checks/views_logins.py
+++ dv_apps/quality_checks/views_logins.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 from django.http import HttpResponse, Http404
 from dv_apps.dataverse_auth.util_logins import UserLoginInfo
 from django.views.decorators.cache import cache_page
 
 
+@cache_page(settings.METRICS_CACHE_VIEW_TIME)
 def view_recent_logins(request):
     """View details about broken notifications"""
 
@@ -12,10 +14,9 @@ def view_recent_logins(request):
                         UserLoginInfo(num_days=100),
                         ]
 
-
     info_dict = dict(page_title="Recent User Logins/API Use",
                      user_login_stats=user_login_stats,
-                     xsubtitle='Broken Notfications')
+                     )
 
     return render(request,
                   'view_recent_logins.html',

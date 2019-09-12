77cb07696dd49b35943468dc6ab4eb181639e1d6
Philip Durbin <philipdurbin@gmail.com>
2019-09-12 08:12:15 -0400

    cleanup (remove files not needed or already elsewhere)

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Installation
-from .models import Institution
-
-# Register your models here.
-
-admin.site.register(Installation)
-admin.site.register(Institution)

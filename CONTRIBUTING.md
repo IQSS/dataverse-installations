2a41aefdcd4c60dcba12b7afbc0eba5e735c5854
Philip Durbin <philip_durbin@harvard.edu>
2020-05-05 15:32:14 -0400

    rewrite update script, docs #70

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -6,39 +6,25 @@ Thank you for your interest in contributing to dataverse-installations!
 
 We are open to contributions from everyone. You don't need to operate an installation of Dataverse to open issues or make suggestions. Please just go ahead! We welcome contributions of ideas, bug reports, feedback, documentation, code, and anything else you can think of.
 
-## Ways to Contribute
-The map of dataverse installations around the world is created from two files that contain various bits of info on each dataverse installations, [crowdsourced spreadsheet][] and the more detailed [spreadsheet maintained by IQSS][].
+## How the map works
 
-You can contribute in one or more of the following ways:
+The [update-data.py][] script downloads data from a [crowdsourced spreadsheet][] to construct a file called [data.json][] that contains lat/long coordinates and other information presented on the map at http://iqss.github.io/dataverse-installations
 
-- Update your installation's information on the [crowdsourced spreadsheet][]
-- Request that your installation info on the [spreadsheet maintained by IQSS][] be updated (include what's new or what's changed) by adding a new issue to our [github issue][]j or by leaving a comment on the [spreadsheet maintained by IQSS][].
-- If you have made (requested) changes to either of the info files, you can update the map by running the map update script (see **Requirements** below).
-   - If you don't want to run the update script, create another [github issue][] to ask someoneelse to update the map.
+## Adding an installation to the map
 
+First, create an issue with information about the installation.
 
-## Updating the Map
+Then, update the [crowdsourced spreadsheet][], requesting edit access if necessary.
 
+Finally, run the update script and make a pull request. Python 3 is required.
 
-### Requirements
-*python 3* OR *docker*
+`python3 update-data.py`
 
-- Fork this dataverse-installations repo to your github
-- Clone your fork onto your local machine and follow the installation instructions to setup your environment
-- Run the update program 
+## Looking at the map locally
 
-    `python3 update-data.py`
+You can see the map at <http://localhost:8000> if you start a web server using the command below.
 
-    OR
-
-    `docker run --rm -v $PWD:/web -w /web -it python:3.7-alpine python3 update-data.py`
-
-    This should update files in the "data" directory.
-
-- Push the updated files to your github "fork"
-- Create a pull request in github
-
-
+`python3 -m http.server`
 
 ### Coding style
 
@@ -52,11 +38,9 @@ If you would like to report a security issue, please email security@dataverse.or
 
 If you have questions or need help, please reach out at [chat.dataverse.org][] or the [dataverse-community Google Group][] or by creating an issue in the [issue tracker][].
 
+[crowdsourced spreadsheet]: https://docs.google.com/spreadsheets/d/1bfsw7gnHlHerLXuk7YprUT68liHfcaMxs1rFciA-mEo/edit#gid=0
+[update-data.py]: update-data.py
+[data.json]: data/data.json
 [chat.dataverse.org]: http://chat.dataverse.org
 [dataverse-community Google Group]: https://groups.google.com/group/dataverse-community
 [issue tracker]: https://github.com/IQSS/dataverse-installations/issues
-[Readme File]: README.md
-
-[spreadsheet maintained by IQSS]:https://docs.google.com/spreadsheets/d/1l2R9D1FQy88qVzg2bI6L1LgplmM2l7pnMI80jdiz4fk/edit?usp=sharing
-[crowdsourced spreadsheet]: https://docs.google.com/spreadsheets/d/1bfsw7gnHlHerLXuk7YprUT68liHfcaMxs1rFciA-mEo/edit#gid=0
-[github issue]: https://github.com/IQSS/dataverse-installations/issues/new
--- update-data.py
+++ update-data.py
@@ -7,71 +7,9 @@ import io
 import os
 
 mydict = {}
+mylist = []
 
-# Crowdsourced data.
-crowd_url = 'https://docs.google.com/spreadsheets/d/1bfsw7gnHlHerLXuk7YprUT68liHfcaMxs1rFciA-mEo/export?gid=0&format=tsv'
-response = urlrequest.urlopen(crowd_url)
-crowd_string = response.read().decode(response.info().get_param('charset') or 'utf-8')
-reader = csv.DictReader(io.StringIO(crowd_string), delimiter="\t")
-rows = [row for row in reader]
-for row in rows:
-    hostname = row['hostname']
-    mydict[hostname] = {}
-    launch_year = row['launch_year']
-    if launch_year:
-        mydict[hostname]['launch_year'] = launch_year
-    else:
-        mydict[hostname]['launch_year'] = None
-    description = row['description']
-    if description:
-        mydict[hostname]['description'] = description
-    else:
-        mydict[hostname]['description'] = None
-
-# Data maintained by IQSS.
-iqss_url = 'https://docs.google.com/spreadsheets/d/1l2R9D1FQy88qVzg2bI6L1LgplmM2l7pnMI80jdiz4fk/export?gid=639378778&format=tsv'
-response = urlrequest.urlopen(iqss_url)
-iqss_string = response.read().decode(response.info().get_param('charset') or 'utf-8')
-reader = csv.DictReader(io.StringIO(iqss_string), delimiter="\t")
-rows = [row for row in reader]
-for row in rows:
-    hostname = row['Installation hostname']
-    country = row['Country']
-    continent = row['Continent']
-    gdcc_member = row['GDCC member']
-    board = row['Project board under IQSS']
-    about_url = row['About URL']
-    core_trust_seals = row['CoreTrustSeals']
-    doi_authority = row['DOI authority']
-    mydict[hostname]['country'] = country
-    mydict[hostname]['continent'] = continent
-    mydict[hostname]['gdcc_member'] = gdcc_member
-    mydict[hostname]['board'] = board
-    mydict[hostname]['about_url'] = about_url
-    mydict[hostname]['core_trust_seals'] = core_trust_seals
-    mydict[hostname]['doi_authority'] = doi_authority
-    mydict[hostname]['contact_email'] = "UNKNOWN"
-
-# Data about harvesting sets, etc.
-harvest_url = 'https://docs.google.com/spreadsheets/d/12cxymvXCqP_kCsLKXQD32go79HBWZ1vU_tdG4kvP5S8/export?gid=0&format=tsv'
-response = urlrequest.urlopen(harvest_url)
-harvest_string = response.read().decode(response.info().get_param('charset') or 'utf-8')
-reader = csv.DictReader(io.StringIO(harvest_string), delimiter="\t")
-rows = [row for row in reader]
-for row in rows:
-    oai_url = row['OAI URL']
-    o = urlparse(oai_url)
-    hostname = o.hostname
-    contact_email = row['Contact email']
-    if contact_email:
-        mydict[hostname]['contact_email'] = contact_email
-    sets = row['Sets']
-    if sets:
-        mydict[hostname]['harvesting'] = sets
-    else:
-        mydict[hostname]['harvesting'] = None
-
-# Data from dataverse.org/metrics (in version of Dataverse is new enough).
+# Data from dataverse.org/metrics (if version of Dataverse is new enough).
 metrics_url = 'https://dataversemetrics.odum.unc.edu/dataverse-metrics/config.json'
 response = urlrequest.urlopen(metrics_url)
 metrics_json = json.loads(response.read().decode(response.info().get_param('charset') or 'utf-8'))
@@ -81,64 +19,65 @@ for i in metrics_json['installations']:
     hostname = o.hostname
     metrics_list.append(hostname)
 
-# Data from miniverse map application.
-miniverse_map_url = 'https://services.dataverse.harvard.edu/miniverse/map/installations-json'
-response = urlrequest.urlopen(miniverse_map_url)
-map_json = json.loads(response.read().decode(response.info().get_param('charset') or 'utf-8'))
-mylist = []
-for i in map_json['installations']:
-    foo = i
-    if not i['is_active']:
+# Crowdsourced data.
+crowd_url = 'https://docs.google.com/spreadsheets/d/1bfsw7gnHlHerLXuk7YprUT68liHfcaMxs1rFciA-mEo/export?gid=0&format=tsv'
+response = urlrequest.urlopen(crowd_url)
+crowd_string = response.read().decode(response.info().get_param('charset') or 'utf-8')
+reader = csv.DictReader(io.StringIO(crowd_string), delimiter="\t")
+rows = [row for row in reader]
+for row in rows:
+    skip = row['skip']
+    if skip:
         continue
-    o = urlparse(i['url'])
-    i['hostname'] = o.hostname
-    if i['hostname'] in metrics_list:
-        i['metrics'] = True
-    if i['hostname'] in mydict:
-        if mydict[i['hostname']]['launch_year']:
-            i['launch_year'] = mydict[i['hostname']]['launch_year']
-        if mydict[i['hostname']]['description']:
-            i['description'] = mydict[i['hostname']]['description']
-        i['country'] = mydict[i['hostname']]['country']
-        i['continent'] = mydict[i['hostname']]['continent']
-        if mydict[i['hostname']]['about_url']:
-            i['about_url'] = mydict[i['hostname']]['about_url']
-        sets = mydict[i['hostname']].get('harvesting')
-        if sets:
-            all_sets = []
-            for harvesting_set in sets.split(','):
-                all_sets.append(harvesting_set.strip())
-            i['harvesting_sets'] = all_sets
-        is_gddc_member = False
-        if mydict[i['hostname']]['gdcc_member'] == 'yes':
-            is_gddc_member = True
-        i['gdcc_member'] = is_gddc_member
-        core_trust_seals = mydict[i['hostname']].get('core_trust_seals')
-        if core_trust_seals:
-            all_seals = []
-            for core_trust_seal in core_trust_seals.split(','):
-                all_seals.append(core_trust_seal)
-            i['core_trust_seals'] = all_seals
-        if mydict[i['hostname']]['doi_authority']:
-            i['doi_authority'] = mydict[i['hostname']]['doi_authority']
-        if mydict[i['hostname']]['board']:
-            i['board'] = mydict[i['hostname']]['board']
-        if mydict[i['hostname']]['contact_email'] != 'UNKNOWN':
-            i['contact_email'] = mydict[i['hostname']]['contact_email']
-    else:
-        print('On the old map only (should be added): ' + i['hostname'])
-    del i['id']
-    del i['is_active']
-    del i['slug']
-    del i['full_name']
-    del i['url']
-    del i['version']
-    del i['logo']
-    mylist.append(i)
+    hostname = row['hostname']
+    name = row['name']
+    mydict[hostname] = {}
+    mydict[hostname]['name'] = name
+    mydict[hostname]['description'] = row['description']
+    latitude = float(row['latitude'])
+    mydict[hostname]['lat'] = latitude
+    longitude = float(row['longitude'])
+    mydict[hostname]['lng'] = longitude
+    mydict[hostname]['hostname'] = hostname
+    if hostname in metrics_list:
+        mydict[hostname]['metrics'] = True
+    launch_year = row['launch_year']
+    if launch_year:
+        mydict[hostname]['launch_year'] = launch_year
+    mydict[hostname]['country'] = row['country']
+    mydict[hostname]['continent'] = row['continent']
+    about_url = row['about_url']
+    if about_url:
+        mydict[hostname]['about_url'] = row['about_url']
+    sets = row['harvesting_sets']
+    if sets:
+        all_sets = []
+        for harvesting_set in sets.split(','):
+            all_sets.append(harvesting_set.strip())
+        mydict[hostname]['harvesting_sets'] = all_sets
+    is_gddc_member = False
+    if row['gdcc_member'] == 'yes':
+        is_gddc_member = True
+    mydict[hostname]['gdcc_member'] = is_gddc_member
+    core_trust_seals = row['core_trust_seals']
+    if core_trust_seals:
+        all_seals = []
+        for core_trust_seal in core_trust_seals.split(','):
+            all_seals.append(core_trust_seal)
+        mydict[hostname]['core_trust_seals'] = all_seals
+    doi_authority = row['doi_authority']
+    if doi_authority:
+        mydict[hostname]['doi_authority'] = doi_authority
+    board = row['board']
+    if board:
+        mydict[hostname]['board'] = board
+    contact_email = row['contact_email']
+    if contact_email:
+        mydict[hostname]['contact_email'] = contact_email
+    mylist.append(mydict[hostname])
 
 final = {}
-final['installations'] = mylist
-#print(json.dumps(final, indent=2))
+final['installations'] = sorted(mylist, key=lambda k: k['name'].lower())
 json_out = os.path.join('data', 'data.json')
 with open(json_out, 'w') as json_out:
     json.dump(final, json_out, indent=2)

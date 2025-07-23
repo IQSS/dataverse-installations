eedcf3c4c886931ec12e02e08c3d08e4e1d0b074
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-07-23 12:27:20 -0400

    adds dv_hub_id to the update-data.py

--- update-data.py
+++ update-data.py
@@ -10,74 +10,81 @@ mydict = {}
 mylist = []
 
 # Data from dataverse.org/metrics (if version of Dataverse is new enough).
-metrics_url = 'https://metrics.dataverse.org/global/config.json'
+metrics_url = "https://metrics.dataverse.org/global/config.json"
 response = urlrequest.urlopen(metrics_url)
-metrics_json = json.loads(response.read().decode(response.info().get_param('charset') or 'utf-8'))
+metrics_json = json.loads(
+    response.read().decode(response.info().get_param("charset") or "utf-8")
+)
 metrics_list = []
-for i in metrics_json['installations']:
+for i in metrics_json["installations"]:
     o = urlparse(i)
     hostname = o.hostname
     metrics_list.append(hostname)
 
 # Crowdsourced data.
-crowd_url = 'https://docs.google.com/spreadsheets/d/1bfsw7gnHlHerLXuk7YprUT68liHfcaMxs1rFciA-mEo/export?gid=0&format=tsv'
+crowd_url = "https://docs.google.com/spreadsheets/d/1bfsw7gnHlHerLXuk7YprUT68liHfcaMxs1rFciA-mEo/export?gid=0&format=tsv"
 response = urlrequest.urlopen(crowd_url)
-crowd_string = response.read().decode(response.info().get_param('charset') or 'utf-8')
+crowd_string = response.read().decode(
+    response.info().get_param("charset") or "utf-8"
+)
 reader = csv.DictReader(io.StringIO(crowd_string), delimiter="\t")
 rows = [row for row in reader]
 for row in rows:
-    skip = row['skip']
+    skip = row["skip"]
     if skip:
         continue
-    hostname = row['hostname']
-    name = row['name']
+    hostname = row["hostname"]
+    name = row["name"]
     mydict[hostname] = {}
-    mydict[hostname]['name'] = name
-    mydict[hostname]['description'] = row['description']
-    latitude = float(row['latitude'])
-    mydict[hostname]['lat'] = latitude
-    longitude = float(row['longitude'])
-    mydict[hostname]['lng'] = longitude
-    mydict[hostname]['hostname'] = hostname
+    mydict[hostname]["name"] = name
+    mydict[hostname]["description"] = row["description"]
+    latitude = float(row["latitude"])
+    mydict[hostname]["lat"] = latitude
+    longitude = float(row["longitude"])
+    mydict[hostname]["lng"] = longitude
+    mydict[hostname]["hostname"] = hostname
     if hostname in metrics_list:
-        mydict[hostname]['metrics'] = True
-    launch_year = row['launch_year']
+        mydict[hostname]["metrics"] = True
+    launch_year = row["launch_year"]
     if launch_year:
-        mydict[hostname]['launch_year'] = launch_year
-    mydict[hostname]['country'] = row['country']
-    mydict[hostname]['continent'] = row['continent']
-    about_url = row['about_url']
+        mydict[hostname]["launch_year"] = launch_year
+    mydict[hostname]["country"] = row["country"]
+    mydict[hostname]["continent"] = row["continent"]
+    about_url = row["about_url"]
     if about_url:
-        mydict[hostname]['about_url'] = row['about_url']
-    sets = row['harvesting_sets']
+        mydict[hostname]["about_url"] = row["about_url"]
+    sets = row["harvesting_sets"]
     if sets:
         all_sets = []
-        for harvesting_set in sets.split(','):
+        for harvesting_set in sets.split(","):
             all_sets.append(harvesting_set.strip())
-        mydict[hostname]['harvesting_sets'] = all_sets
+        mydict[hostname]["harvesting_sets"] = all_sets
     is_gddc_member = False
-    if row['gdcc_member'] == 'yes':
+    if row["gdcc_member"] == "yes":
         is_gddc_member = True
-    mydict[hostname]['gdcc_member'] = is_gddc_member
-    core_trust_seals = row['core_trust_seals']
+    mydict[hostname]["gdcc_member"] = is_gddc_member
+    core_trust_seals = row["core_trust_seals"]
     if core_trust_seals:
         all_seals = []
-        for core_trust_seal in core_trust_seals.split(','):
+        for core_trust_seal in core_trust_seals.split(","):
             all_seals.append(core_trust_seal)
-        mydict[hostname]['core_trust_seals'] = all_seals
-    doi_authority = row['doi_authority']
+        mydict[hostname]["core_trust_seals"] = all_seals
+    doi_authority = row["doi_authority"]
     if doi_authority:
-        mydict[hostname]['doi_authority'] = doi_authority
-    board = row['board']
+        mydict[hostname]["doi_authority"] = doi_authority
+    board = row["board"]
     if board:
-        mydict[hostname]['board'] = board
-    contact_email = row['contact_email']
+        mydict[hostname]["board"] = board
+    contact_email = row["contact_email"]
     if contact_email:
-        mydict[hostname]['contact_email'] = contact_email
+        mydict[hostname]["contact_email"] = contact_email
+    dv_hub_id = row["dv_hub_id"]
+    if dv_hub_id:
+        mydict[hostname]["dv_hub_id"] = dv_hub_id
     mylist.append(mydict[hostname])
 
 final = {}
-final['installations'] = sorted(mylist, key=lambda k: k['name'].lower())
-json_out = os.path.join('data', 'data.json')
-with open(json_out, 'w') as json_out:
+final["installations"] = sorted(mylist, key=lambda k: k["name"].lower())
+json_out = os.path.join("data", "data.json")
+with open(json_out, "w") as json_out:
     json.dump(final, json_out, indent=2)
